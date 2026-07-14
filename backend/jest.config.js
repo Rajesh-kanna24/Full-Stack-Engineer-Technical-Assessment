1d62eb239d4f5b13e35b36cbaef04d585283227a
rajeshkanna-stumpmic <rajesh.kanna.stumpmic.ai>
2026-07-14 19:53:53 +0530

    Fix environment variables, remove duplicate libs, and track frontend

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,10 +1,7 @@
-// jest.config.ts
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // This line tells Jest to run our setup file before running any tests
   setupFiles: ['<rootDir>/jest.setup.ts'],
-  // If you are using path aliases like @/ in your backend, include this:
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
